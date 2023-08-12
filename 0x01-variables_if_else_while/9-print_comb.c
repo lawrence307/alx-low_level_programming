730eafeed0541ec9fc375b3b5dfe6e3018fb8d9f
lawrence307 <lawrence_muthini@yahoo.com>
2023-08-12 12:38:22 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,10 @@
+#include <stdio.h>
 /*
  * File: 9-print_comb.c
- * Auth: Brennan D Baraban
+ * Auth: Lawrence M Nzomo
  */
-
-#include <stdio.h>
-
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main: Prints all possible combination of single-digit numbers
  *
  * Return: Always 0.
  */
@@ -14,17 +12,14 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for(num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
-
 	return (0);
 }
