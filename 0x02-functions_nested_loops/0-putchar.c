2531b28d65a1325682cd325dc6ef56c2c6f765ed
lawrence307 <lawrence_muthini@yahoo.com>
2023-08-15 05:09:09 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0_putchar.c
+ * Auth: Lawrence M Nzomo
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	putchar('_');
+        putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
