2101d07cf51b3db455245fbaf18b02a8168d6d3b
lawrence307 <lawrence_muthini@yahoo.com>
2023-09-11 12:53:14 -0700

    function that prints its file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints name of the file
- *
- * Return: Always 0 (success)
+ * main - prints the name of file
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%s|n",  __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
